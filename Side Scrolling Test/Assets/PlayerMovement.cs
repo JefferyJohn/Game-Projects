71132ad9c8d184c876d4570e114bb6141ddd2a45
Jeffery John <53410876+JefferyJohn@users.noreply.github.com>
2020-03-29 05:03:41 -0400

    Created Side Scrolling Test
    Updated Unity to 2019.3.7f1

---
+++ Side Scrolling Test/Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+// https://github.com/Brackeys/2D-Character-Controller
+// https://github.com/prime31/CharacterController2D/blob/master/Assets/CharacterController2D/CharacterController2D.cs
+// 
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    
+}
